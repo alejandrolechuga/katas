cef6f1c0321c83745e1a166762577604820e30b3
Alejandro Lechuga <ing.ramon.alejandro@gmail.com>
2017-03-04 23:55:39 -0500

    add kata 1
    Format a string of names like 'Bart, Lisa & Maggie'

---
+++ scripts/kata1.js
@@ -0,0 +1,43 @@
+/***
+Given: an array containing hashes of names
+
+Return: a string formatted as a list of names separated by commas except for the last two names, which should be separated by an ampersand.
+
+Example:
+
+list([ {name: 'Bart'}, {name: 'Lisa'}, {name: 'Maggie'} ])
+// returns 'Bart, Lisa & Maggie'
+
+list([ {name: 'Bart'}, {name: 'Lisa'} ])
+// returns 'Bart & Lisa'
+
+list([ {name: 'Bart'} ])
+// returns 'Bart'
+
+list([])
+// returns ''
+
+**/
+
+function list(names) {
+  var string; 
+  var last;
+  var ampersand = ' & ';
+  var comma = ', ';
+  
+  names = names
+    .map(function(item){
+      return item.name;
+    });
+    
+  if ( names.length > 2) {
+    last = names.pop();
+    string = names.join(comma) + [ampersand, last].join('');
+  } else if ( names.length > 1)  {
+    string = names.join(ampersand);
+  } else {
+    string = names[0] || '';
+  }
+  
+  return string;
+}
